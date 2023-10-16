82c4c21a7efba08af23d868eea96cb57bd6afea0
Shashank Dhakate <shashankdkte@gmail.com>
2023-10-16 06:29:08 +0530

    Writing Data with React Router Actions

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
-
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurent";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -33,13 +34,14 @@ const fakeCart = [
 
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
+  const navigation = useNavigation();
   const cart = fakeCart;
 
   return (
     <div>
       <h2>Ready to order? Lets go!</h2>
 
-      <form>
+      <Form method="POST">
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -71,11 +73,26 @@ function CreateOrder() {
         </div>
 
         <div>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)}/>
           <button>Order now</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
 }
 
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority:data.priority === 'on'
+  }
+
+  const newOrder = await createOrder(order);
+  console.log(newOrder);
+  return redirect(`/order/${newOrder.id}`);
+}
 export default CreateOrder;
